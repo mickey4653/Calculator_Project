d3decaff33e93ced337a9b0972a0a3fd971b79fd
mickey4653 <adeniyimichaelid@gmail.com>
2021-07-17 04:00:53 +0100

    started the project but got stuck but we go over it later

--- calcScript.js
+++ calcScript.js
@@ -1,28 +1,34 @@
 // function add(n1, n2) {
-//     return n1 + n2;
+    // return n1 + n2;
 
 // }
 
 // function substract(n1, n2) {
-//     return n1 - n2;
+    // return n1 - n2;
 
 // }
 
 // function multiply(n1, n2) {
-//     return n1 * n2;
+    // return n1 * n2;
 
 // }
 
 // function divide(n1, n2) {
-//     return n1 / n2;
+    // return n1 / n2;
 
 // }
 
 // function operate(operator, n1, n2) {
-//     if (operator === "+") {
-//         return add(n1, n2);
+    // if (operator === "+") {
+        // return add(n1, n2);
 
-//     }
+    // }else if(operator === "-"){
+		// return substract(n1, n2);
+	// }else if(operator === "*"){
+		// return multiply(n1, n2);
+	// }else if(operator === "/"){
+		// return divide(n1, n2);
+	// }
 
 // }
 var displayVal = '0';
